cffe454653b12a701289a2e63f075fd19fd0475e
lassandra85 <lassandra85@gmail.com>
2022-12-09 20:04:31 +0100

    add

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,7 @@
-const BASE_URL = 'https://restcountries.com';
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
 
 const fetchCountries = function (name) {
-  return fetch(`${BASE_URL}/v3.1/name/${name}?fields=name,capital,population,flags,languages`).then(
+  return fetch(`${BASE_URL}${name}?fields=name,capital,population,flags,languages`).then(
     response => {
       if (response.status === 404) {
         return Promise.reject(new Error());
